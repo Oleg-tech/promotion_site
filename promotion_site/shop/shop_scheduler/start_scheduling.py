69e01d74af8df9cc78895c1b446add4059e280cd
Urchick Oleg <olegurchick@gmail.com>
2022-07-16 15:18:37 +0300

    pagination and images added

--- promotion_site/shop/shop_scheduler/start_scheduling.py
+++ promotion_site/shop/shop_scheduler/start_scheduling.py
@@ -5,5 +5,5 @@ from shop.shop_scheduler.get_products_from_zakaz import main_parse
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(main_parse, 'cron', hour='16', minute='28')
+    scheduler.add_job(main_parse, 'cron', hour='9', minute='52')
     scheduler.start()
